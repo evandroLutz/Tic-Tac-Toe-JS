d91c498def3a570167235097f13c3d77cb5d0bb3
Evandro Lutz <evandrolutz@outlook.com>
2020-12-26 03:30:49 -0300

    method to check victory

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let turnPlayer = 0;
 
+let winner = false;
+
+const winGrade = [
+   ['0', '1', '2'],
+   ['3', '4', '5'],
+   ['6','7','8'],
+   ['2', '4', '6'],
+   ['0','4','8'],
+   ['0','3','6'],
+   ['1','4','7'],
+   ['2','5','8']   
+]
+
 function listenerToggleOnSquares(){
     const squares = document.querySelectorAll('.container .square');
     squares.forEach((square)=>{
@@ -18,12 +31,28 @@ function controlPlayerTurn(turnActual){
 
 function togglePlayerChar(char,square){
     const turnChar = char === 'x'? square.querySelector('.x') : square.querySelector('.o');
-    if(!checkAlreadyFilled(square)){
+    if(!checkAlreadyFilled(square) && !winner){
         turnChar.classList.add('show');
         turnPlayer++;
+        checkWin(char);
     }    
 }
 
 function checkAlreadyFilled(square){
     return square.querySelector('.show');
+}
+
+
+function checkWin(char){
+    const charSquares = [];
+    const charShoweds = document.querySelectorAll(`.${char}.show`);
+    charShoweds.forEach((charShowed)=>{
+       charSquares.push(charShowed.parentNode.id);
+    });
+    winGrade.forEach((possibility)=>{
+        if(JSON.stringify(charSquares)==JSON.stringify(possibility)){
+            console.log(`${char} winner`);
+            winner = true;
+        }
+    })
 }
